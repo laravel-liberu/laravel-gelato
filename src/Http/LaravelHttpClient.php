28e16bed671ecde15c9ad9fe37dc7b635e691825
xdimension <xdim222@gmail.com>
2021-12-27 20:17:41 +0700

    use Illuminate\Http\Response

--- src/Http/LaravelHttpClient.php
+++ src/Http/LaravelHttpClient.php
@@ -2,7 +2,7 @@
 namespace iWebTouch\Gelato\Http;
 
 use Illuminate\Support\Facades\Http;
-use Illuminate\Http\Client\Response;
+use Illuminate\Http\Response;
 
 class LaravelHttpClient implements HttpClient
 {
